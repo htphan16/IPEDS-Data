1f6d03b627c203af6ca21f649e38d6f56bd20001
Huong Phan <htphan16@earlham.edu>
2019-04-14 19:10:39 -0400

    Add files via upload

--- t-test_small_vs_big.R
+++ t-test_small_vs_big.R
@@ -20,11 +20,29 @@ college2016 <- fread('2016_data.csv', header=TRUE)
 college2017 <- fread('2017_data.csv', header=TRUE)
 
 # 2017
+fr2017_all2$"Full-time retention rate" <- as.integer(fr2017_all2$"Full-time retention rate")
+fr2017_all2 <- college2017 %>% drop_na("Full-time retention rate") %>% select('Full-time retention rate', 'Institution size Category', 'Sector of institution')
+fr2017_new <- subset(fr2017_all2, fr2017_all2$'Full-time retention rate' > 20)
 
-fr2017_all2 <- college2017 %>% drop_na("Full-time retention rate") %>% select('Full-time retention rate', 'Institution size Category')
-fr2017_all2$"`Institution size Category`" <- as.numeric(fr2017_all$"`Institution size Category`")
+fr2017_new
+hist(fr2017_new$'Full-time retention rate', breaks=20)
 
-fr2017_all2[which(fr2017_all2$"Institution size Category" <= 2)]$'Full-time retention rate'
-fr2017_all2[which(fr2017_all2$"Institution size Category" >= 3)]$'Full-time retention rate'
+fr2017_new$"Institution size Category" <- as.numeric(fr2017_new$"Institution size Category")
 
-t.test(sample(fr2017_all2[which(fr2017_all2$"Institution size Category" <= 2)]$'Full-time retention rate', 50), sample(fr2017_all2[which(fr2017_all2$"Institution size Category" >= 3)]$'Full-time retention rate', 50))
+fr2017_new$"Sector of institution" <- as.numeric(fr2017_new$"Sector of institution")
+
+fr2017_new$'Full-time retention rate'
+
+# Only consider public and private 4-year universities
+fr2017_new[which((fr2017_new$"Institution size Category" >= 0) & (fr2017_new$"Institution size Category" <= 2) & (fr2017_new$"Sector of institution" <= 3) & (fr2017_new$"Sector of institution" >= 1))]$'Full-time retention rate'
+fr2017_new[which((fr2017_new$"Institution size Category" >= 3) & (fr2017_new$"Sector of institution" <= 3) & (fr2017_new$"Sector of institution" >= 1))]$'Full-time retention rate'
+
+hist(fr2017_new[which((fr2017_new$"Institution size Category" >= 0) & (fr2017_new$"Institution size Category" <= 2) & (fr2017_new$"Sector of institution" <= 3) & (fr2017_new$"Sector of institution" >= 1))]$'Full-time retention rate', breaks=20)
+hist(fr2017_new[which((fr2017_new$"Institution size Category" >= 3) & (fr2017_new$"Sector of institution" <= 3) & (fr2017_new$"Sector of institution" >= 1))]$'Full-time retention rate', breaks=20)
+
+t.test(sample(fr2017_new[which((fr2017_new$"Institution size Category" >= 0) & (fr2017_new$"Institution size Category" <= 2) & (fr2017_new$"Sector of institution" <= 3) & (fr2017_new$"Sector of institution" >= 1))]$'Full-time retention rate', 100), sample(fr2017_new[which((fr2017_new$"Institution size Category" >= 3) & (fr2017_new$"Sector of institution" <= 3) & (fr2017_new$"Sector of institution" >= 1))]$'Full-time retention rate', 100), var.equal = FALSE)
+
+t.test(fr2017_new[which((fr2017_new$"Institution size Category" >= 0) & (fr2017_new$"Institution size Category" <= 2) & (fr2017_new$"Sector of institution" <= 3) & (fr2017_new$"Sector of institution" >= 1))]$'Full-time retention rate', fr2017_new[which((fr2017_new$"Institution size Category" >= 3) & (fr2017_new$"Sector of institution" <= 3) & (fr2017_new$"Sector of institution" >= 1))]$'Full-time retention rate', var.equal = FALSE, alternative = c("less"))
+
+t.test(fr2017_new[which((fr2017_new$"Institution size Category" >= 0) & fr2017_new$"Institution size Category" <= 2)]$'Full-time retention rate', fr2017_new[which(fr2017_new$"Institution size Category" >= 3)]$'Full-time retention rate', var.equal = FALSE)
+
